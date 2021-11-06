a89d8295fac2143cf1d7b65ba369a14a0bee85e3
Xu Han <ufo.1997@qq.com>
2021-11-06 22:36:45 +0800

    finish ch2

---
+++ 5.py
@@ -0,0 +1,8 @@
+num1 = int(input("num1 = ? "))
+num2 = int(input("num2 = ? "))
+
+print(f"num1 + num2 = {num1+num2}")
+print(f"num1 - num2 = {num1-num2}")
+print(f"num1 * num2 = {num1*num2}")
+print(f"num1 / num2 = {num1/num2}")
+print(f"num1 % num2 = {num1%num2}")
